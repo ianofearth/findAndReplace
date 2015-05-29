ec063d5ea4b3f449e275e48464a9dfe60a6f8a5a
ianofearth <ian.anthony.bevel>
2015-05-29 10:10:06 -0700

    abandon find word method since it can be incorporated in a single method, write first spec to test findAndReplace method

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
-var findWord = function(phrase, wordToFind) {
-	var splitPhrase = phrase.split(" ");
-	var wordToFind = wordToFind
-	var foundWord = []
-	splitPhrase.forEach(function(word) {
-		if(wordToFind === word) {
-			foundWord.push(word)
-
-		}
-	});return foundWord
-
+var findAndReplace = function(phrase, wordToReplace, replaceWith) {
+	var splitPhrase = phrase.split(" ")
 }
+
+
+
+
+// var findWord = function(phrase, wordToFind) {
+// 	var splitPhrase = phrase.split(" ");
+// 	var wordToFind = wordToFind
+// 	var foundWord = []
+// 	splitPhrase.forEach(function(word) {
+// 		if(wordToFind === word) {
+// 			foundWord.push(word)
+
+// 		}
+// 	});return foundWord
+
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,12 @@
-describe ("findWord", function (phrase, wordToFind) {
-	it("will find a specific word from an input string", function(){
-		expect(findWord("ian is cool", "ian")).to.eql(["ian"]);
+// describe ("findWord", function (phrase, wordToFind) {
+// 	it("will find a specific word from an input string", function(){
+// 		expect(findWord("ian is cool", "ian")).to.eql(["ian"]);
+// 	});
+// });
+
+describe("findAndReplace", function (phrase, wordToReplace, replaceWith) {
+	it("will replace found word in phrase with a desired word", function() {
+		expect(findAndReplace("ian is cool", "cool", "awesome")).to.equal("ian is awesome");
 	});
+
 });
