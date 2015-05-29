0f1f60e749f4a548b2196ca9cf20f238960d877e
ianofearth <ian.anthony.bevel>
2015-05-29 10:30:09 -0700

    pass spec to replace single instance of a word

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 var findAndReplace = function(phrase, wordToReplace, replaceWith) {
-	var splitPhrase = phrase.split(" ")
+	var phrase = phrase;
+	var wordToReplace = wordToReplace;
+	var replaceWith = replaceWith;
+	var replaced = phrase.replace(wordToReplace, replaceWith);
+	return replaced;
 }
 
+// var findAndReplace = function(phrase, wordToReplace, replaceWith) {
+// 	var splitPhrase = phrase.split(" ")
+// 	var wordToReplace = wordToReplace
+// 	var replaceWith = replaceWith
+// 	splitPhrase.forEach(function(word) {
+// 		if(wordToReplace === word) {
+// 			splitPhrase.replace(word, replaceWith)
+// 		}
 
+// 	});
+// 	return splitPhrase.join();
+
+// }
 
 
 // var findWord = function(phrase, wordToFind) {
