26ae45d418ade854f44b5627ff4a3b26fadad83d
ianofearth <ian.anthony.bevel>
2015-05-29 09:59:08 -0700

    pass find word method

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 var findWord = function(phrase, wordToFind) {
-	var splitString = string.split(" ");
+	var splitPhrase = phrase.split(" ");
 	var wordToFind = wordToFind
+	var foundWord = []
+	splitPhrase.forEach(function(word) {
+		if(wordToFind === word) {
+			foundWord.push(word)
+
+		}
+	});return foundWord
+
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,5 +2,4 @@ describe ("findWord", function (phrase, wordToFind) {
 	it("will find a specific word from an input string", function(){
 		expect(findWord("ian is cool", "ian")).to.eql(["ian"]);
 	});
-
 });
